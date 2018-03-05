26521f5da2ea111b32f7b99ecebeb004a5f948d1
Yohei Tsuji <tsuji.y.ae@m.titech.ac.jp>
2018-03-05 18:51:34 +0900

    Update

---
+++ dlframeworks/chainer/datasets/__init__.py
@@ -0,0 +1,5 @@
+from dlframeworks.chainer.datasets import image_dataset # NOQA
+
+
+from dlframeworks.chainer.datasets.image_dataset import CroppingDataset  # NOQA
+from dlframeworks.chainer.datasets.image_dataset import read_pairs  # NOQA
