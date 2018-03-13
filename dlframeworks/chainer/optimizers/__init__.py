3172571c663e23a68a29ce50aeb94fb6423958fc
Yohei Tsuji <tsuji.y.ae@m.titech.ac.jp>
2018-03-13 13:31:33 +0900

    Update

--- dlframeworks/chainer/optimizers/__init__.py
+++ dlframeworks/chainer/optimizers/__init__.py
@@ -1,6 +1,10 @@
 from dlframeworks.chainer.optimizers import rmsprop_warmup  # NOQA
+from dlframeworks.chainer.optimizers import kfac  # NOQA
 
 
+from dlframeworks.chainer.optimizers.kfac import KFAC  # NOQA
+from dlframeworks.chainer.optimizers.kfac import KFACUpdater  # NOQA
+from dlframeworks.chainer.optimizers.kfac import KFACUpdateRule  # NOQA
 from dlframeworks.chainer.optimizers.rmsprop_warmup import RMSpropWarmup  # NOQA
 from dlframeworks.chainer.optimizers.rmsprop_warmup import RMSpropWarmupRule  # NOQA
 from dlframeworks.chainer.optimizers.rmsprop_warmup import RMSpropWarmupScheduler  # NOQA
