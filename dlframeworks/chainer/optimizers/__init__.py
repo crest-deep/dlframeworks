a66f2e56dc5dc7e41a9df08fa3e2f1c5cbfc0083
Yohei Tsuji <t.t.t.yohei@gmail.com>
2018-03-20 14:36:31 +0900

    Update

--- dlframeworks/chainer/optimizers/__init__.py
+++ dlframeworks/chainer/optimizers/__init__.py
@@ -1,9 +1,13 @@
 from dlframeworks.chainer.optimizers import rmsprop_warmup  # NOQA
 from dlframeworks.chainer.optimizers import kfac  # NOQA
+from dlframeworks.chainer.optimizers import kfac_communication  # NOQA
 
 
 from dlframeworks.chainer.optimizers.kfac import KFAC  # NOQA
 from dlframeworks.chainer.optimizers.kfac import KFACUpdateRule  # NOQA
+from dlframeworks.chainer.optimizers.kfac_communication import allreduce_grad  # NOQA
+from dlframeworks.chainer.optimizers.kfac_communication import allreduce_cov  # NOQA
+from dlframeworks.chainer.optimizers.kfac_communication import bcast_inv  # NOQA
 from dlframeworks.chainer.optimizers.rmsprop_warmup import RMSpropWarmup  # NOQA
 from dlframeworks.chainer.optimizers.rmsprop_warmup import RMSpropWarmupRule  # NOQA
 from dlframeworks.chainer.optimizers.rmsprop_warmup import RMSpropWarmupScheduler  # NOQA
