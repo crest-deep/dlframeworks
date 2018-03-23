d266bfe0a978ca712bd5b4e1efecdb5d658428e6
Yohei Tsuji <tsuji.y.ae@m.titech.ac.jp>
2018-03-23 12:48:50 +0900

    Add test code

--- dlframeworks/chainer_tests/communicators_tests/test_kfac_communicator.py
+++ dlframeworks/chainer_tests/communicators_tests/test_kfac_communicator.py
@@ -8,6 +8,7 @@ import mock
 import unittest
 from mpi4py import MPI
 import numpy as np
+import cupy as cp
 
 import dlframeworks
 
@@ -25,119 +26,181 @@ class MLP(chainer.Chain):
         return F.sum(self.l2(h))  # output MUST be 1 dim
 
 
-class KFACCommunicatorTest(unittest.TestCase):
+class Obj(object):
 
-    def setUp(self):
-        self.mpi_comm = MPI.COMM_WORLD
+    def __init__(self, comm_name):
+        self.comm_name = comm_name
         self.comm = dlframeworks.chainer.communicators.KFACCommunicator(
-            self.mpi_comm, 'naive')
-
-    def test_allreduce_grad(self):
-        model = MLP()
-        optimizer = chainermn.create_multi_node_optimizer(
+            MPI.COMM_WORLD, comm_name)
+        self.device = self.comm.intra_rank
+        chainer.cuda.get_device(self.device).use()
+        self.model = MLP().to_gpu()
+        self.optimizer = chainermn.create_multi_node_optimizer(
             chainer.optimizers.Adam(), self.comm)
-        optimizer.setup(model)
-        y = model(np.array([[1, 2]], dtype=np.float32))
+        self.optimizer.setup(self.model)
+
+    def __call__(self, x):
+        y = self.model(x)
         y.backward()
 
-        for i in range(2):
-            if self.comm.gcomm.rank == 0:
-                for param in model.params():
-                    shape = param.data.shape
-                    param.data = np.ones(shape, dtype=np.float32)
-                    param.grad = np.ones(shape, dtype=np.float32)
+
+def generate_random_arrays(shape, n, index):
+    arrays = []
+    for i in range(n):
+        array = cp.ones(shape).astype(cp.float32) * (i + 1)
+        arrays.append(array)
+
+    for i in range(n):
+        mean_array = cp.zeros(shape).astype(cp.float32)
+        mean_array += arrays[i]
+    mean_array /= n
+
+    return mean_array, arrays[0], arrays[index]
+
+
+class KFACCommunicatorTest(unittest.TestCase):
+
+    @parameterized.expand([
+        'pure_nccl',
+        'hierarchical',
+        'two_dimensional',
+        'flat',
+        'naive',
+    ])
+    def test_allreduce_grad(self, comm_name):
+        obj = Obj(comm_name)
+        obj(cp.array([[1, 2]], dtype=cp.float32))
+
+        if not obj.comm.is_grad_worker:
+            return
+
+        mean_arrays = {}
+        first_arrays = {}
+        for name, param in sorted(obj.model.namedparams()):
+            shape = param.data.shape
+            mean_array, first_array, array = generate_random_arrays(
+                shape, obj.comm.gcomm.size, obj.comm.gcomm.rank)
+            param.data = array
+            param.grad = array
+            mean_arrays[name] = mean_array
+            first_arrays[name] = first_array
+
+        synced = obj.comm.allreduce_grad(obj.optimizer)
+
+        for name, param in sorted(obj.model.namedparams()):
+            if synced:
+                cp.testing.assert_array_almost_equal(
+                    param.grad, mean_arrays[name])
             else:
-                for param in model.params():
-                    shape = param.data.shape
-                    param.data = np.zeros(shape, dtype=np.float32)
-                    param.grad = np.ones(shape, dtype=np.float32)
+                cp.testing.assert_array_almost_equal(
+                    param.data, first_arrays[name])
 
-            synced = self.comm.allreduce_grad(optimizer)
+    @parameterized.expand([
+        'pure_nccl',
+        'hierarchical',
+        'two_dimensional',
+        'flat',
+        'naive',
+    ])
+    def test_bcast_inv(self, comm_name):
+        obj = Obj(comm_name)
+        obj(cp.array([[1, 2]], dtype=cp.float32))
 
-            if self.comm.is_grad_worker:
-                for param in model.params():
-                    shape = param.data.shape
-                    if synced:
-                        self.assertTrue(np.all(param.grad == 1),
-                                        'expected 1, however {}'.format(param.grad[0]))
-                    else:
-                        self.assertTrue(np.all(param.data == 1),
-                                        'expected 1, however {}'.format(param.data[0]))
+        if not obj.comm.is_grad_worker or not obj.comm.is_inv_worker:
+            return
 
-    def test_bcast_inv(self):
-        model = MLP()
         invs = collections.OrderedDict()
+        first_arrays = {}
         for name, _ in model.namedparams():
-            if self.comm.gcomm_g.rank == 0:
-                invs[name] = np.ones((4, 4), dtype=np.float32)
-            else:
-                invs[name] = np.zeros((4, 4), dtype=np.float32)
+            _, first_array, array = generate_random_arrays(
+                (4, 4), obj.comm.gcomm_g.size, obj.comm.gcomm_g.rank)
+            invs[name] = [array, array + 1]
+            first_arrays[name] = first_array
 
         self.comm.bcast_inv(invs)
 
-        if self.comm.is_grad_worker:
-            for name, inv in invs.items():
-                self.assertTrue(np.all(inv == 1),
-                                'expected 1, however {}'.format(inv[0]))
+        for name, matrices in invs.items():
+            for i, matrix in enumerate(matrices):
+                cp.testing.assert_array_almost_equal(
+                    matrix, first_array[name] + i)
 
-    def test_allreduce_cov(self):
-        covs = [np.ones((4, 4), dtype=np.float32),
-                np.ones((4, 4), dtype=np.float32)]
+    @parameterized.expand([
+        'pure_nccl',
+        'hierarchical',
+        'two_dimensional',
+        'flat',
+        'naive',
+    ])
+    def test_allreduce_cov(self, comm_name):
+        obj = Obj(comm_name)
+        obj(cp.array([[1, 2]], dtype=cp.float32))
+
+        if not obj.comm.is_cov_worker:
+            return
+
+        covs = []
+        mean_arrays = []
+        for i in range(3):
+            mean_array, _, array = generate_random_arrays(
+                (4, 4), obj.comm.ccomm.size, obj.comm.ccomm.rank)
+            covs.append(array)
+            mean_arrays.append(mean_array)
         
-        self.comm.allreduce_cov(covs)
+        obj.comm.allreduce_cov(covs)
 
-        if self.comm.is_cov_worker:
-            for cov in covs:
-                self.assertTrue(np.all(cov == 1 ),
-                                'expected {} however {}'.format(
-                                    1 , cov[0]))
+        for i in range(3):
+            cp.testing.assert_array_almost_equal(
+                covs[i], mean_arrays[i])
 
-    def test_sendrecv_param(self):
-        model = MLP()
-        optimizer = chainermn.create_multi_node_optimizer(
-            chainer.optimizers.Adam(), self.comm)
-        optimizer.setup(model)
-        y = model(np.array([[1, 2]], dtype=np.float32))
-        y.backward()
+    @parameterized.expand([
+        'pure_nccl',
+        'hierarchical',
+        'two_dimensional',
+        'flat',
+        'naive',
+    ])
+    def test_sendrecv_param(self, comm_name):
+        obj = Obj(comm_name)
+        obj(cp.array([[1, 2]], dtype=cp.float32))
 
-        if self.comm.is_grad_master:
-            for param in model.params():
-                shape = param.data.shape
-                param.data = np.ones(shape, dtype=np.float32)
-        else:
-            for param in model.params():
-                shape = param.data.shape
-                param.data = np.zeros(shape, dtype=np.float32)
+        if not obj.comm.is_grad_master or not obj.comm.is_cov_worker:
+            return
 
-        synced = self.comm.sendrecv_param(optimizer)
+        first_arrays = {}
+        for name, param in sorted(obj.model.namedparams()):
+            shape = param.data.shape
+            _, first_array, array = generate_random_arrays(
+                shape, 2, obj.comm.is_grad_master)
+            param.data = array
+            param.grad = array
+            first_arrays[name] = first_array
 
-        if self.comm.is_cov_worker or self.comm.is_grad_master:
-            self.assertTrue(np.all(param.data == 1),
-                            'expected 1, however {}'.format(param.data[0]))
-        else:
-            self.assertTrue(np.all(param.data == 0),
-                            'expected 0, however {}'.format(param.data[0]))
+        obj.comm.sendrecv_param(optimizer)
 
-    def test_sendrecv_cov_ema(self):
-        model = MLP()
-        cov_emas = collections.OrderedDict()
-        for name, _ in model.namedparams():
-            if self.comm.is_cov_worker:
-                cov_emas[name] = np.ones((4, 4), dtype=np.float32)
-            else:
-                cov_emas[name] = None
+        for name, param in sorted(obj.model.namedparams()):
+                cp.testing.assert_array_almost_equal(
+                    param.data, first_arrays[name])
 
-        self.comm.sendrecv_cov_ema(cov_emas)
-
-        if self.comm.is_inv_worker:
-            self.assertTrue(len(cov_emas) == len(list(model.namedparams())),
-                            'expected {} however {}'.format(
-                            len(list(model.namedparams())),
-                            len(cov_emas)))
-            for name, cov_ema in cov_emas.items():
-                self.assertTrue(np.all(cov_ema == 1),
-                                'expected 1, however {}'.format(cov_ema[0]))
-
+#    def test_sendrecv_cov_ema(self):
+#        model = MLP()
+#        cov_emas = collections.OrderedDict()
+#        for name, _ in model.namedparams():
+#            if self.comm.is_cov_worker:
+#                cov_emas[name] = np.ones((4, 4), dtype=np.float32)
+#            else:
+#                cov_emas[name] = None
+#
+#        self.comm.sendrecv_cov_ema(cov_emas)
+#
+#        if self.comm.is_inv_worker:
+#            self.assertTrue(len(cov_emas) == len(list(model.namedparams())),
+#                            'expected {} however {}'.format(
+#                            len(list(model.namedparams())),
+#                            len(cov_emas)))
+#            for name, cov_ema in cov_emas.items():
+#                self.assertTrue(np.all(cov_ema == 1),
+#                                'expected 1, however {}'.format(cov_ema[0]))
+#
 
 def main():
     unittest.main()
